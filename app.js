3bc4991d40eb6f475f90dfc99cb241b9a73c7126
Garrett <garry@g-brown.net>
2024-06-19 13:43:13 -0400

    Adde 100.9 Coast Country

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
 const audio1 = new Audio("https://audio-edge-qse4n.yyz.g.radiomast.io/2ce0b08d-2fe6-42a1-b64a-9f0a682f5508");
 const audio4 = new Audio("https://ais-edge90-dal03.cdnstream.com/b14981_128mp3?listenerId=esTrackblock0629183&aw_0_1st.playerid=esPlayer&aw_0_1st.skey=1717349341");
+const audio6 = new Audio("https://ice64.securenetsystems.net/WMJK");
 
 function playSong(id) {
     var buttonID  = document.getElementById(id);
@@ -84,7 +85,22 @@ function playSong(id) {
                 document.getElementById("lofi").setAttribute("src", "https://www.youtube.com/embed/BZe7cC_xDio")
             return;
             }
-
+//100.9 Coast Country
+        case "song6":
+            if (buttonID.getAttribute("data-playing") == "true") {
+                console.log("Stopping audio...")
+                audio6.pause();
+                buttonID.setAttribute("data-playing", "false")
+                containerID.setAttribute("selected", "false")
+                return;
+                
+            }else if (buttonID.getAttribute("data-playing") == "false") {
+                console.log("Playing audio...")
+                audio6.play()
+                buttonID.setAttribute("data-playing", "true")
+                containerID.setAttribute("selected", "true")
+                return;
+            }
 
     }
 }
